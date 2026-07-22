efad30ce7edacd9491207456fa01ae974eda0cf1
wzz <1761365378@qq.com>
2026-07-22 18:41:55 +0800

    1f

--- src/main/java/com/wzz/lobotocraft/entity/abnormality/EntityRedShoes.java
+++ src/main/java/com/wzz/lobotocraft/entity/abnormality/EntityRedShoes.java
@@ -217,9 +217,7 @@ public class EntityRedShoes extends AbstractAbnormality {
                 "血之渴望",
                 "特殊",
                 "red_shoes_curio",
-                "压迫工作完成后获得「内在的勇气」。",
-                "持有「内在的勇气」完成三次压迫工作后转化为「匹夫之勇」。",
-                "携带者进行沟通工作时会被处决。"
+                "最大生命值 + 4"
         );
     }
 
